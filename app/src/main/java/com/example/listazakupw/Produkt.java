baf1d210b0eb3850790bf4ce8f4e958075253161
k3mil <kamilszkolka123456789@gmail.com>
2026-02-06 07:37:08 +0100

    żarówki

--- app/src/main/java/com/example/listazakupw/Produkt.java
+++ app/src/main/java/com/example/listazakupw/Produkt.java
@@ -1,4 +1,34 @@
 package com.example.listazakupw;
 
 public class Produkt {
+    private String nazwaProduktu;
+    private Boolean czyZakupiono;
+
+    public Produkt(String nazwaProduktu) {
+        this.nazwaProduktu = nazwaProduktu;
+        czyZakupiono = false;
+    }
+
+    @Override
+    public String toString() {
+        return "Produkt{" +
+                "nazwaProduktu='" + nazwaProduktu + '\'' +
+                '}';
+    }
+
+    public String getNazwaProduktu() {
+        return nazwaProduktu;
+    }
+
+    public void setNazwaProduktu(String nazwaProduktu) {
+        this.nazwaProduktu = nazwaProduktu;
+    }
+
+    public Boolean getCzyZakupiono() {
+        return czyZakupiono;
+    }
+
+    public void setCzyZakupiono(Boolean czyZakupiono) {
+        this.czyZakupiono = czyZakupiono;
+    }
 }
---
+++ app/src/main/java/com/example/listazakupw/ZakupyAdapter.java
@@ -0,0 +1,8 @@
+package com.example.listazakupw;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ZakupyAdapter extends RecyclerView.Adapter<ZakupyAdapter.ProduktViewHolder> {
+    public class ProduktViewHolder {
+    }
+}
